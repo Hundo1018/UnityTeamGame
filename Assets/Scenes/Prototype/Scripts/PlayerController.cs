078334438e82fc247e2fc4df2de96214f2f418e9
Qin-Yi Yang <t108590028@ntut.edu.tw>
2021-01-20 10:34:35 +0800

    PlayerSkillOK

--- Assets/Scenes/Prototype/Scripts/PlayerController.cs
+++ Assets/Scenes/Prototype/Scripts/PlayerController.cs
@@ -3,38 +3,13 @@ using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using SkillConstructer;
 
 public class PlayerController : MonoBehaviour
 {
-    /// <summary>
-    /// 測試用3x3 T型 小技能
-    /// </summary>
-    static bool O = true;
-    static bool X = false;
-    int[] ability =
-    {
-        0b_1_0_1,
-        0b_0_1_0,
-        0b_1_0_1
-    };
-    /// <summary>
-    /// 測試用5x5 T型 大技能
-    /// </summary>
-    int[] ultimateAbility =
-    {
-        0b_0_0_1_0_0,
-        0b_0_1_1_1_0,
-        0b_1_1_1_1_1,
-        0b_0_1_1_1_0,
-        0b_0_0_1_0_0        
-    };
-
-    /// <summary>
-    /// 開完技能後要清除的位置
-    /// </summary>
-    List<Vector2Int> abilityClears;
-    int abilityClears_Num = 0;
-
+    Skill Ability = new Skill("101;010;101");
+    Skill UltimateAbility = new Skill("00100;01110;11111;01110;00100");
+    
     public StageManager stageManager;
     Vector2Int now = new Vector2Int(2, 2);
     /// <summary>
@@ -45,127 +20,6 @@ public class PlayerController : MonoBehaviour
     int hor = 0;
     bool space = false;
     bool pressJ = false;
-    /// <summary>
-    /// 清掉技能用到的stage
-    /// </summary>
-    void ClearStages()
-    {
-        //abilityClears.ForEach(v => stageManager.SetStatus(v, false));
-        //abilityClears = new List<Vector2Int>();
-        stageManager.ClearAllStatus(abilityClears_Num);
-        abilityClears_Num = 0;
-    }
-
-    /// <summary>
-    /// 檢查是否可以放小招，之後應該優化成，只檢查有true的部分
-    /// </summary>
-    /// <returns></returns>
-    bool CheckAbility()
-    {
-        bool[] tmp_IsOk = new bool[9];
-        bool flag = false;
-        //int counter = 0;
-        int startY = Mathf.Clamp(now.y - 2, 0, 4);
-        int startX = Mathf.Clamp(now.x - 2, 0, 4);
-        int tmp_getSkill, tmp_getStatus;
-        tmp_getSkill = 0;
-        for (int y = 0; y < 3; y++)
-        {
-            tmp_getSkill = (tmp_getSkill << 5) + ability[y];
-        }
-        tmp_getSkill = (tmp_getSkill << 12);
-        tmp_getStatus = Convert.ToInt32(String.Join("", stageManager.GetAllStatus().Select(x=>x?1:0).ToArray()),2);
-
-        for(int y=0,i=0; y<3; y++){
-            for(int x=0; x<3; x++,i++){
-                tmp_IsOk[i] = (tmp_getSkill & tmp_getStatus) == tmp_getSkill;
-                tmp_getSkill = tmp_getSkill >> 1;
-            }
-            tmp_getSkill = tmp_getSkill >> 2;
-        }
-
-        abilityClears_Num = 0;
-        for(int i = startY*3+startX, counter=0; counter<9; i=(i+1)%9,counter++){
-            if(tmp_IsOk[i])
-            {
-                for (int y = 0; y < 3; y++)
-                {
-                    abilityClears_Num = (abilityClears_Num << 5) + ability[y];
-                }
-                abilityClears_Num = abilityClears_Num << 12;
-                abilityClears_Num = abilityClears_Num >> (i/3)*5 + i%3;
-                return true;
-            }
-        }
-        /*while (counter < 9)
-        {
-            flag = false;
-            abilityClears = new List<Vector2Int>();
-            for (int y = 0; y < 3; y++)
-            {
-                if (flag) break;
-                for (int x = 0; x < 3; x++)
-                {
-                    if (ability[y, x])
-                    {
-                        if (stageManager.stageStatus[startY + y, startX + x])
-                        {
-                            abilityClears.Add(new Vector2Int(startX + x, startY + y));
-                        }
-                        else
-                        {
-                            flag = true;
-                            break;
-                        }
-                    }
-                    if (x == y && x == 2) return true;
-
-                }
-            }
-            startX++;
-            if (startX > 2)
-            {
-                startX = 0;
-                startY++;
-                if (startY > 2)
-                {
-                    startY = 0;
-                }
-            }
-            counter++;
-        }//*/
-        return false;
-    }
-    /// <summary>
-    /// 檢查是否可以放大招，之後應該優化成，只檢查有true的部分
-    /// </summary>
-    /// <returns></returns>
-    bool CheckUltimateAbility()
-    {
-        int tmp_getSkill = 0, tmp_getStatus;
-        for (int y = 0; y < 5; y++)
-        {
-            tmp_getSkill = (tmp_getSkill << 5) + ultimateAbility[y];
-        }
-        //用於取得技能需觸發範圍
-        
-        tmp_getStatus = Convert.ToInt32(String.Join("", stageManager.GetAllStatus().Select(x=>x?1:0).ToArray()),2);
-        //用於取得場上格子狀態
-        //a.Select(x=>x?1:0).ToArray 將a的元素True轉為1，False轉為0
-        //String,Join(a,b) 以aㄗㄨㄛ
-        //Convert.ToInt32(a,2) ur;
-
-        if((tmp_getSkill & tmp_getStatus) == tmp_getSkill)//判斷格子狀態是否覆蓋技能需觸發範圍True為技能觸發成功
-        {
-            abilityClears_Num = tmp_getSkill;
-            return true;
-        }
-        else
-        {
-            abilityClears_Num = 0;
-            return false;
-        }
-    }
 
     /// <summary>
     /// 更新狀態
@@ -183,34 +37,33 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))//技能判斷
         {
-            if (CheckUltimateAbility())
+            if (UltimateAbility.Attack(now, stageManager.GetAllStatus()))
             {
                 Debug.Log("Ultimate!");
-                ClearStages();
-
             }
-            else if (CheckAbility())
+            else if (Ability.Attack(now, stageManager.GetAllStatus()))
             {
                 Debug.Log("Ability!");
-                ClearStages();
             }
             else
             {
                 Debug.Log("Fail!");
             }
-        }else if(Input.GetKeyDown(KeyCode.J)){//格子觸發判斷
+        }
+        else if (Input.GetKeyDown(KeyCode.J))
+        {//格子觸發判斷
             stageManager.SetStatus(now, true);
         }
         else//移動判斷
         {
-            if(Input.GetKeyDown(KeyCode.W)) now.y -= 1;
-            if(Input.GetKeyDown(KeyCode.S)) now.y += 1;
-            
-            if(Input.GetKeyDown(KeyCode.A)) now.x -= 1;
-            if(Input.GetKeyDown(KeyCode.D)) now.x += 1;
+            if (Input.GetKeyDown(KeyCode.W)) now.y -= 1;
+            if (Input.GetKeyDown(KeyCode.S)) now.y += 1;
+
+            if (Input.GetKeyDown(KeyCode.A)) now.x -= 1;
+            if (Input.GetKeyDown(KeyCode.D)) now.x += 1;
             //把數值限制在0~4
-            now.x = Mathf.Clamp(now.x , 0, 4);
-            now.y = Mathf.Clamp(now.y , 0, 4);
+            now.x = Mathf.Clamp(now.x, 0, 4);
+            now.y = Mathf.Clamp(now.y, 0, 4);
             updateStatus();
         }
     }
@@ -223,7 +76,7 @@ public class PlayerController : MonoBehaviour
             Input.GetKeyDown(KeyCode.A) ||
             Input.GetKeyDown(KeyCode.S) ||
             Input.GetKeyDown(KeyCode.D) ||
-            Input.GetKeyDown(KeyCode.Space)||
+            Input.GetKeyDown(KeyCode.Space) ||
             Input.GetKeyDown(KeyCode.J)) return true;
         return false;
     }
@@ -232,6 +85,14 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         updateStatus();
+        Ability.DoAttackForEachCube = (x, y) =>
+        {
+            stageManager.SetStatus(x, y, false);
+        };
+        UltimateAbility.DoAttackForEachCube = (x, y) =>
+        {
+            stageManager.SetStatus(x, y, false);
+        };
     }
 
     // Update is called once per frame
--- Assets/Scenes/Prototype/Scripts/Skill.cs
+++ Assets/Scenes/Prototype/Scripts/Skill.cs
@@ -1,293 +1,320 @@
 ﻿using System;
 using UnityEngine;
 
-public class Skill
+namespace SkillConstructer
 {
-    private int _w, _h;  //觸發圖形大小
-    private int _shape; //觸發圖形 或 攻擊範圍
-
-    private int _type;  //攻擊類型 0 玩家技能判定 1 以絕對座標攻擊 2 以隨機座標攻擊 3 以相對座標攻擊
-
-    private int _data;  //攻擊數據(隨Tpye變化) 0 無數據 1 攻擊數量 2 基準位置
-    private int _nextRange;//下次攻擊範圍
-
-    //隨機生成亂數用
-    private double rand
+    public class Skill
     {
-        get
+        private int _w, _h;  //觸發圖形大小
+        private int _shape; //觸發圖形 或 攻擊範圍
+
+        private int _type;  //攻擊類型 0 玩家技能判定 1 以絕對座標攻擊 2 以隨機座標攻擊 3 以相對座標攻擊
+
+        private int _data;  //攻擊數據(隨Tpye變化) 0 無數據 1 攻擊數量 2 基準位置
+        private int _nextRange;//下次攻擊範圍
+
+        //隨機生成亂數用
+        private double rand
         {
-            return UnityEngine.Random.value;
-        }
-    }
-
-    //攻擊時要做的動作
-    public Action<int, int> DoAttackForEachCube;
-    public Action DoAttackOnce;
-
-    //初始化變數用
-    private void Init(int w, int h, int shape, int type, int data)
-    {
-        w = _w; h = _h;
-        _shape = shape;
-        _type = type;
-        _data = data;
-        CreateNextRange();
-    }
-
-    private void CreateNextRange()
-    {
-        //攻擊範圍(固定式)
-        if (_type == 1) _nextRange = _shape;
-        //攻擊範圍(隨機式)
-        else if (_type == 2)
-        {
-            int max = _data / 2, cou = 0, i = 0;
-            if (_data % 2 == 0)
+            get
             {
-                _nextRange = 0;
-                //隨機生成
-                while (i < 25)
-                {
-                    _nextRange <<= 1;
-                    i++;
-                    if (rand * 25 < max)
-                    {
-                        _nextRange += 1;
-                        cou++;
-                    }
-                    //確認是否達到上限
-                    if (cou == max) break;
-                }
-                //填滿25格
-                _nextRange <<= 24 - i;
-                //過濾範圍外位置
-                _nextRange &= _shape;
+                return UnityEngine.Random.value;
             }
-            else
+        }
+
+        //攻擊時要做的動作
+        public Action<int, int> DoAttackForEachCube = (x, y) => { };
+        public Action DoAttackOnce = () => { };
+
+        //初始化變數用
+        private void Init(int w, int h, int shape, int type, int data)
+        {
+            _w = w; _h = h;
+            _shape = shape;
+            _type = type;
+            _data = data;
+            CreateNextRange();
+        }
+
+        private void CreateNextRange()
+        {
+            //攻擊範圍(固定式)
+            if (_type == 1) _nextRange = _shape;
+            //攻擊範圍(隨機式)
+            else if (_type == 2)
             {
-                int tempRange = 1;
+                int max = _data / 2, cou = 0, i = 0;
+                if (_data % 2 == 0)
+                {
+                    _nextRange = 0;
+                    //隨機生成
+                    while (i < 25)
+                    {
+                        _nextRange <<= 1;
+                        i++;
+                        if (rand * 25 < max)
+                        {
+                            _nextRange += 1;
+                            cou++;
+                        }
+                        //確認是否達到上限
+                        if (cou == max) break;
+                    }
+                    //填滿25格
+                    _nextRange <<= 24 - i;
+                    //過濾範圍外位置
+                    _nextRange &= _shape;
+                }
+                else
+                {
+                    int tempRange = 1;
+                    _nextRange = _shape;
+                    //計算場上可釋放的格子數量
+                    while (i < 25)
+                    {
+                        tempRange <<= 1;
+                        if (_nextRange % 2 == 1)
+                        {
+                            tempRange += 1;
+                            cou++;
+                        }
+                        _nextRange >>= 1;
+                        i++;
+                    }
+                    //判斷上限是否超過格子數
+                    if (max > cou) max = cou;
+                    //隨機生成
+                    i = 0;
+                    while (i < max)
+                    {
+                        _nextRange <<= 1;
+                        if (tempRange % 2 == 1)
+                        {
+                            //判斷是否剩餘格子數不夠
+                            if (cou == max - i)
+                            {
+                                _nextRange += 1;
+                                i++;
+                            }
+                            else if (rand * 25 < max)
+                            {
+                                _nextRange += 1;
+                                i++;
+                            }
+                            cou--;
+                        }
+                        tempRange >>= 1;
+                    }
+                    //填滿25格
+                    while (tempRange > 1)
+                    {
+                        _nextRange <<= 1;
+                        tempRange >>= 1;
+                    }
+                }
+            }
+            //攻擊範圍(追蹤式)
+            else if (_type == 3)
+            {
+                int[] temp = new int[25];
                 _nextRange = _shape;
-                //計算場上可釋放的格子數量
-                while (i < 25)
+                int dy = 7 - (_data / 5), dx = 7 - (_data % 5);
+                for (int i = 4; i >= 0; i--)
                 {
-                    tempRange <<= 1;
-                    if (_nextRange % 2 == 1)
+                    for (int j = 4; j >= 0; j--)
                     {
-                        tempRange += 1;
-                        cou++;
+                        temp[((i + dy) * 5 + ((j + dx) % 5)) % 25] = _nextRange % 2;
+                        _nextRange >>= 1;
                     }
-                    _nextRange >>= 1;
-                    i++;
                 }
-                //判斷上限是否超過格子數
-                if (max > cou) max = cou;
-                //隨機生成
-                i = 0;
-                while (i < max)
+                for (int i = 0; i < 25; i++)
                 {
                     _nextRange <<= 1;
-                    if (tempRange % 2 == 1)
-                    {
-                        //判斷是否剩餘格子數不夠
-                        if (cou == max - i)
-                        {
-                            _nextRange += 1;
-                            i++;
-                        }
-                        else if (rand * 25 < max)
-                        {
-                            _nextRange += 1;
-                            i++;
-                        }
-                        cou--;
-                    }
-                    tempRange >>= 1;
-                }
-                //填滿25格
-                while (tempRange > 1)
-                {
-                    _nextRange <<= 1;
-                    tempRange >>= 1;
+                    _nextRange += temp[i];
                 }
             }
+            //例外狀況
+            else _nextRange = 0;
         }
-        //攻擊範圍(追蹤式)
-        else if (_type == 3)
+
+        //空技能
+        public Skill()
         {
-            int[] temp = new int[25];
-            _nextRange = _shape;
-            int dy = 7 - (_data / 5), dx = 7 - (_data % 5);
-            for (int i = 4; i >= 0; i--)
+            Init(0, 0, 0, -1, 0);
+        }
+        
+        //建構玩家技能
+        //shape是以每一Row以;分隔
+        public Skill(string shape)
+        {
+            string[] str = shape.Split(';');
+            int h = str.Length, w = 0, tmp_shape = 0;
+            if (h > 5) h = 5;
+            
+            for (int i = 0; i < h; i++)
             {
-                for (int j = 4; j >= 0; j--)
+                if (w < str[i].Length)
                 {
-                    temp[((i + dy) * 5 + ((j + dx) % 5)) % 25] = _nextRange % 2;
-                    _nextRange >>= 1;
+                    w = str[i].Length;
+                    if (w > 5) w = 5;
+                }
+                str[i] += "00000";
+                for (int j = 0; j < 5; j++)
+                {
+                    tmp_shape <<= 1;
+                    if (str[i][j] != '0') tmp_shape += 1;
                 }
             }
+            tmp_shape <<= (5 - h) * 5;
+            Init(w, h, tmp_shape, 0, 0);
+        }
+
+        //建構固定式敵人攻擊
+        //range是以25bits來表示的攻擊範圍
+        public Skill(int range)
+        {
+            Init(0, 0, range, 1, 0);
+        }
+
+        //建構隨機式敵人攻擊
+        //range是以25bits來表示的攻擊範圍
+        //type 0是小於等於numberOfCube 1是等於numberOfCube
+        //numberOfCube是召喚的上限
+        public Skill(int range, int type, int numberOfCube)
+        {
+            Init(0, 0, range, 2, (numberOfCube << 2) + type);
+        }
+
+        //建構追蹤式敵人攻擊
+        //range是以25bits來表示的相對攻擊範圍
+        //position是玩家的位置
+        public Skill(int range, Vector2Int position)
+        {
+            Init(0, 0, range, 3, position.y * 5 + position.x);
+        }
+
+        //更改隨機式攻擊的數據
+        //type 0是小於等於numberOfCube 1是等於numberOfCube
+        //numberOfCube是召喚的上限
+        public void ChangeData(int type, int numberOfCube)
+        {
+            Init(0, 0, _shape, 2, (numberOfCube << 2) + type);
+        }
+
+        //更改追蹤式攻擊的數據
+        //position是玩家的位置
+        public void ChangeData(Vector2Int position)
+        {
+            Init(0, 0, _shape, 3, position.y * 5 + position.x);
+        }
+
+        //判斷技能是否觸發
+        public bool isTrigger(bool[] mapStatus)
+        {
+            int status = 0;
+            _nextRange = _shape;
             for (int i = 0; i < 25; i++)
             {
-                _nextRange <<= 1;
-                _nextRange += temp[i];
+                status <<= 1;
+                if (mapStatus[i]) status += 1;
             }
-        }
-        //例外狀況
-        else _nextRange = 0;
-    }
-
-    //空技能
-    public Skill()
-    {
-        Init(0, 0, 0, -1, 0);
-    }
-
-    //建構玩家技能
-    //shape是以二維陣列來表示的判定範圍
-    public Skill(bool[][] shape)
-    {
-        int h = shape.Length, w, tmp_shape = 0;
-        if (h == 0) w = 0;
-        else w = shape[0].Length;
-        for (int i = 0; i < h; i++)
-        {
-            for (int j = 0; j < w; j++)
+            for (int i = 0; i < 6 - _h; i++)
             {
-                tmp_shape <<= 1;
-                if (shape[i][j]) tmp_shape += 1;
+                for (int j = 0; j < 6 - _w; j++)
+                {
+                    if ((status & _nextRange) == _nextRange) return true;
+                    _nextRange >>= 1;
+                }
+                _nextRange >>= _w;
             }
-            tmp_shape <<= 5;
+            return false;
         }
-        tmp_shape <<= (5 - h) * 5 + (5 - w);
-        Init(w, h, tmp_shape, 0, 0);
-    }
 
-    //建構固定式敵人攻擊
-    //range是以25bits來表示的攻擊範圍
-    public Skill(int range)
-    {
-        Init(0, 0, range, 1, 0);
-    }
-
-    //建構隨機式敵人攻擊
-    //range是以25bits來表示的攻擊範圍
-    //type 0是小於等於numberOfCube 1是等於numberOfCube
-    //numberOfCube是召喚的上限
-    public Skill(int range, int type, int numberOfCube)
-    {
-        Init(0, 0, range, 2, (numberOfCube << 2) + type);
-    }
-
-    //建構追蹤式敵人攻擊
-    //range是以25bits來表示的相對攻擊範圍
-    //position是玩家的位置
-    public Skill(int range, Vector2Int position)
-    {
-        Init(0, 0, range, 3, position.y * 5 + position.x);
-    }
-
-    //更改隨機式攻擊的數據
-    //type 0是小於等於numberOfCube 1是等於numberOfCube
-    //numberOfCube是召喚的上限
-    public void ChangeData(int type, int numberOfCube)
-    {
-        Init(0, 0, _shape, 2, (numberOfCube << 2) + type);
-    }
-
-    //更改追蹤式攻擊的數據
-    //position是玩家的位置
-    public void ChangeData(Vector2Int position)
-    {
-        Init(0, 0, _shape, 3, position.y * 5 + position.x);
-    }
-
-    //判斷技能是否觸發
-    public bool isTrigger(bool[] mapStatus)
-    {
-        int status = 0;
-        _nextRange = _shape;
-        for (int i = 0; i < 25; i++)
+        //取得敵人攻擊範圍
+        public int getAttackRange()
         {
-            status <<= 1;
-            if (mapStatus[i]) status += 1;
+            if (_type == 0) _nextRange = _shape;
+            return _nextRange;
         }
-        for (int i = 0; i < 6 - _h; i++)
+
+        //取得敵人攻擊範圍 並計算下次攻擊位置
+        public int getAttackRangeWithChange()
         {
-            for (int j = 0; j < 6 - _w; j++)
+            int temp = _nextRange;
+            CreateNextRange();
+            return _nextRange;
+        }
+
+        //敵人用攻擊
+        public void Attack()
+        {
+            int attackRange = getAttackRangeWithChange();
+            int i = 24;
+            while (attackRange != 0)
             {
-                if ((status & _nextRange) == _nextRange) return true;
-                _nextRange >>= 1;
+                //攻擊範圍內再動作
+                if (attackRange % 2 == 1)
+                {
+                    DoAttackForEachCube(i % 5, i / 5);
+                }
+                attackRange >>= 1;
+                i -= 1;
             }
-            _nextRange >>= _w;
+            DoAttackOnce();
         }
-        return false;
-    }
 
-    //取得敵人攻擊範圍
-    public int getAttackRange()
-    {
-        return _nextRange;
-    }
-
-    //取得敵人攻擊範圍 並計算下次攻擊位置
-    public int getAttackRangeWithChange()
-    {
-        int temp = _nextRange;
-        CreateNextRange();
-        return _nextRange;
-    }
-
-    public void Attack()
-    {
-        int attackRange = getAttackRangeWithChange();
-        int i = 24;
-        while (attackRange != 0)
+        //玩家用攻擊
+        public bool Attack(Vector2Int position, bool[] mapStatus)
         {
-            if (attackRange % 2 == 1)
+            int status = 0, w = 6 - _w, h = 6 - _h, size = w * h;
+            int pos = (position.y - _h + 1) * w + (position.x - _w + 1);
+            bool[] isOK = new bool[size];
+            _nextRange = _shape;
+            //讀取地圖狀態
+            for (int i = 0; i < 25; i++)
             {
-                DoAttackForEachCube(i / 5, i % 5);
+                status <<= 1;
+                if (mapStatus[i]) status += 1;
             }
-            attackRange /= 0;
-            i -= 1;
-        }
-        DoAttackOnce();
-    }
-
-    public void Attack(Vector2Int position, bool[] mapStatus)
-    {
-        int status = 0, w = 6 - _w, h = 6 - _h, size = w * h;
-        int pos = (position.y - _h + 1) * w + (position.x - _w + 1);
-        bool[] isOK = new bool[size];
-        _nextRange = _shape;
-        for (int i = 0; i < 25; i++)
-        {
-            status <<= 1;
-            if (mapStatus[i]) status += 1;
-        }
-        for (int i = 0; i < h; i++)
-        {
-            for (int j = 0; j < w; j++)
+            //確認符合區域
+            for (int i = 0; i < h; i++)
             {
-                isOK[i * w + j] = ((status & _nextRange) == _nextRange);
-                _nextRange >>= 1;
-
+                for (int j = 0; j < w; j++)
+                {
+                    isOK[i * w + j] = ((status & _nextRange) == _nextRange);
+                    _nextRange >>= 1;
+                }
+                _nextRange >>= _w-1;
             }
-            _nextRange >>= _w;
-        }
-        while (pos < 0) pos += w;
-        for (int i = 0; i < size; i++)
-        {
-            if (isOK[(pos + i) % size]) {
-                pos = (pos + i) % size;
-                break;
-            }
-        }
-        for (int i = pos / w; i < _h; i++)
-        {
-            for (int j = pos % w; j < _w; j++)
+            //尋找適當的位置
+            while (pos < 0) pos += w;
+            for (int i = 0; i < size; i++)
             {
-                DoAttackForEachCube(i, j);
+                if (isOK[(pos + i) % size])
+                {
+                    pos = (pos + i) % size;
+                    break;
+                }
             }
+            //執行動作
+            if (isOK[pos])
+            {
+                int shape = _shape;
+                shape >>= (pos / w) * 5 + (pos % w);
+                int i = 24;
+                while (shape != 0)
+                {
+                    if (shape % 2 == 1)
+                    {
+                        DoAttackForEachCube(i % 5, i / 5);
+                    }
+                    shape >>= 1;
+                    i -= 1;
+                }
+                DoAttackOnce();
+                return true;
+            }
+            return false;
         }
-        DoAttackOnce();
     }
 }
